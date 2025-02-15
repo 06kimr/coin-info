f7b7e01dcb5796e0fcbdde04e75a5bd270fab15c
rickykim-99 <youngs.kim@creadto.com>
2025-02-15 12:52:05 +0900

    [FEAT] 암호 화폐 목록 출력 및 prefetch 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
+import { getQueryClient } from '@/utils'
+import { QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [query_client] = useState(getQueryClient())
+
+  return <QueryClientProvider client={query_client}><Component {...pageProps} /></QueryClientProvider>
 }
